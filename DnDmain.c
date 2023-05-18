714f2d562489f5f4dcb64afc08d2a18b04a9129c
Xander <12203308@student.pxl.be>
2023-05-18 21:17:48 +0200

    Fifth commit, a possible bug has been fixed

--- DnDmain.c
+++ DnDmain.c
@@ -1,6 +1,7 @@
 #include "DnDparse.h"
 
 
+
 struct newNode
 {
 	char *index;
@@ -14,59 +15,43 @@ struct newNode
 	struct newNode *next;
 };
 
+void printHead( struct newNode *head );
+
 int main(void)
 {
 	
 	char *file = "SpellResult.json";
-	parse(file);
-	
-	
-	/*
 	struct Result tempResult = parse(file);
 	struct newNode *head = NULL;
-	newNode.index = tempResult.index;
-	newNode.name = tempResult.name;
-	newNode.url = tempResult.url;
-	newNode.range = tempResult.range;
-	newNode.material = tempResult.material;
-	newNode.duration = tempResult.duration;
-	newNode.casting_time = tempResult.casting_time;
 	
-	for (int i = 0; i < 2; i++)
-	{
-		if( head == NULL )
-		{
-			head = (struct newNode *)calloc( 1, sizeof( struct newNode ) );
-			head->index = tempResult.index;
-			head->name = tempResult.name;
-			head->url = tempResult.url;
-			head->range = tempResult.range;
-			head->material = tempResult.material;
-			head->duration = tempResult.duration;
-			head->casting_time = tempResult.casting_time;
-			head->next = head;
-		}
-		else
-		{
-			struct newNode *pointer = (struct newNode *)calloc( 1, sizeof( struct newNode ) );
-			file = "acid-splash.json";
-			tempResult = parse(file);
-			
-			pointer->index = tempResult.index;
-			pointer->name = tempResult.name;
-			pointer->url = tempResult.url;
-			pointer->range = tempResult.range;
-			pointer->material = tempResult.material;
-			pointer->duration = tempResult.duration;
-			pointer->casting_time = tempResult.casting_time;
-			pointer->next = head;
-			
-		}
-		
-	}
+	head = (struct newNode *)calloc( 1, sizeof( struct newNode ) );
+	head->index = tempResult.index;
+	printf("index = %s\n", head->index);
+	head->name = tempResult.name;
+	printf("name = %s\n", head->name);
+	head->url = tempResult.url;
+	printf("url = %s\n", head->url);
+	head->range = tempResult.range;
+	printf("range = %s\n", head->range);
+	head->material = tempResult.material;
+	printf("material = %s\n", head->material);
+	head->duration = tempResult.duration;
+	printf("duration = %s\n", head->duration);
+	head->casting_time = tempResult.casting_time;
+	printf("casting_time = %s\n", head->casting_time);
+	head->next = head;
 	
+	printf("\n\nindex = %s\n", head->index);
 	//free(pointer);
+	printHead(head);
 	free(head);
-	*/
+	
 	return 0;
 }
+
+void printHead( struct newNode *head )
+{
+	printf("\n\nindex = %s\n", head->index);
+}
+
+
